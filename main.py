f6663ee74df339ad1a9680bd142ea3c47eae995e
Ed French <edwardmfrench@gmail.com>
2023-11-09 13:50:11 +0000

    Server controls implemented

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import cv2
+from flask import Flask,send_file
+import threading
 
 FILENAME="media/snowman_nobars.mp4"
 COUNTDOWN="media/countdown.mp4"
@@ -19,14 +21,28 @@ KINGS_HALL={"zoom":0.75,"fullscreen_x_offset":-1024,"fullscreen_y_offset":630}
 KEY_RIGHT_CODE=2555904
 KEY_LEFT_CODE=2424832
 
+class MockPlayer:
+    timecode="mm:ss:ff"
+    proportion=0.0
+
+g_player=MockPlayer() # Singleton, temporary version to ensure web interface works
+
+
 class Player:
-    def __init__(self,filename):
+    def __init__(self,filename,title=""):
         self.filename=filename
         self.frame_no:int=0
         self.frame_noplaying:bool=False
         self.cap=None
         self.length_frames=-1
         self.fps=-1
+        self.timecode="??:??:??"
+        self.proportion=0.0
+        self.pause_requested=False
+        self.restart_requested=False
+        self.countdown_requested=False
+        self.skip_requested=False
+        self.title=title
 
 
 
@@ -40,6 +56,8 @@ class Player:
 
         # cv2.CV_CAP_PROP_FRAME_WIDTH)   # float `width`
         self.height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT) ) # float `height`
+        self.proportion=0.0
+        self.timecode="00:00:00"
 
     @staticmethod
     def wait_for(ordinal):
@@ -52,6 +70,7 @@ class Player:
 
 
     def play_clip(self,zoom=0.5,fullscreen_x_offset=-1920,fullscreen_y_offset=630,supress_second_monitor=False,last_frame=None)->bool:
+
         self.playing=True
         if self.cap is None:
             self.reset()
@@ -97,18 +116,25 @@ class Player:
                     cv2.namedWindow(controlwin,cv2.WINDOW_NORMAL)
                     cv2.resizeWindow(controlwin,int(self.width),int(self.height))
 
-                    proportion=self.frame_no/self.length_frames
+                    self.proportion=self.frame_no/self.length_frames
 
-                    propx=int((self.width-5)*proportion)
+                    propx=int((self.width-5)*self.proportion)
                     propxplus=int(propx+5)
-
+                    
                     seconds_in=self.frame_no/self.fps
+                    if self.title=="Countdown":
+                        seconds_in=self.length_frames/self.fps-seconds_in+1
+                        
+                    
                     whole_secs=int(seconds_in)
                     fraction=int((seconds_in-whole_secs)*100)
                     minutes=int(whole_secs/60)
                     seconds=whole_secs-60*minutes
 
-                    cv2.putText(frame,f"{minutes:02}:{seconds:02}:{fraction:02}",(20,200),0,4,(255,255,0),4)
+                    self.timecode=f"{minutes:02}:{seconds:02}:{fraction:02}"
+
+
+                    cv2.putText(frame,self.timecode,(20,200),0,4,(255,255,0),4)
 
                     cv2.rectangle(frame,(propx,int(0)),(propxplus,int(self.height-1)),(255,255,0),3)
 
@@ -119,28 +145,68 @@ class Player:
             fullpress=cv2.waitKeyEx(25)
             key= fullpress & 0xFF
             print(self.frame_no,fullpress,key)
-            if  key== ord('q') or key==27: 
+
+
+            # Skip the track
+
+            if  key== ord('s') or key==27: 
                 break
-            elif key== ord('s'):
+
+            elif g_player.skip_requested:
+                g_player.skip_requested=False
+                break
+
+            
+            # Restart the track
+
+            elif key== ord('r'):
                 self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0-1)
                 self.frame_no=0
+
+            elif g_player.restart_requested:
+                g_player.restart_requested=False
+                self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0-1)
+                self.frame_no=0
+
+
+            # close the application
+
             elif key == ord('z'):
                 self.cap.release()
                 cv2.destroyAllWindows()
                 return False
+            
+
+
             elif fullpress==KEY_LEFT_CODE:
                 self.frame_no=max(0,self.frame_no-60)
                 self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.frame_no-1)
             elif fullpress==KEY_RIGHT_CODE:
                 self.frame_no=min(self.length_frames-1,self.frame_no+60)
                 self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.frame_no-1)
-
-            elif key ==32:
+            
+            
+            # Toggle pausing playback
+            elif key ==32: # Space bar
                 # Pausing until released
                 self.wait_for(0xFF)
                 self.playing=not self.playing
 
+            elif self.pause_requested:
+                self.playing=not self.playing
+                self.pause_requested=False
                         
+
+            # Back to countdown
+
+            elif self.countdown_requested:
+                self.countdown_requested=False
+                if self.title=="Countdown":
+                    self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0-1)
+                    self.frame_no=0
+                else:
+                    g_player.skip_requested=False
+                    break
 
 
         
@@ -153,18 +219,97 @@ class Player:
         # Closes all the frames 
         cv2.destroyAllWindows() 
         return True # Ended normally
+    
 
 
-while True:
-    clip=Player(COUNTDOWN)
-    clip.reset()
+app=Flask(__name__)
+@app.route("/")
+def main():
+    return send_file("webif.html")
 
-    res=clip.play_clip(last_frame=30*11,supress_second_monitor=True)
-    if not res:
-        break
+@app.route("/timecode")
+def timecode():
+    raw=f"""    <div hx-target="this"
+        hx-get="/timecode"
+        hx-trigger="load delay:200ms"
+        hx-swap="outerHTML">
+            <div id="title">{g_player.title}</div>
+            <span id="timecode">{g_player.timecode}</span>
+          <div class="progresscontainer">  
+            <div style="width:{g_player.proportion*100}%" class="progressbar"></div>
+          </div>  
+    </div>"""
+    return raw
 
-    clip=Player(FILENAME)
-    clip.reset()
-    res=clip.play_clip(**KINGS_HALL)
-    if not res:
-        break
+@app.route("/pause")
+def pause():
+    g_player.pause_requested=True
+    return   """<button 
+                            hx-get="/pause"
+                            hx-target="this"
+                            hx-trigger="click"
+                            accesskey="p"
+                            hx-swap="outerHTML">
+            Pause
+        </button>"""
+
+@app.route("/restart")
+def restart():
+    g_player.restart_requested=True
+    return """<button class="active"
+                accesskey="R"
+                hx-get="/restart"
+                hx-target="this"
+                hx-trigger="click"
+                hx-swap="outerHTML">
+            Restart
+        </button>"""
+
+@app.route("/countdown")
+def countdown():
+    g_player.countdown_requested=True
+    return """        <button class="active"
+                accesskey="C"
+                hx-get="/countdown"
+                hx-target="this"
+                hx-trigger="click"
+                hx-swap="outerHTML">
+            Countdown
+        </button>"""
+
+@app.route("/skip")
+def skip():
+    g_player.skip_requested=True
+
+
+
+    return """        <button class="active"
+                    accesskey="S"
+                    hx-get="/skip"
+                    hx-target="this"
+                    hx-trigger="click"
+                    hx-swap="outerHTML">
+            Skip
+        </button>"""
+
+if __name__=="__main__":
+
+    
+    # Launch server
+    server=threading.Thread(target=app.run,daemon=True,kwargs={"host":"0.0.0.0", "port":80})
+    server.start()
+        
+    while True:
+        g_player=Player(COUNTDOWN,"Countdown")
+        g_player.reset()
+
+        res=g_player.play_clip(last_frame=30*11,supress_second_monitor=True)
+        if not res:
+            break
+
+        g_player=Player(FILENAME,"Snowman")
+        g_player.reset()
+        res=g_player.play_clip(**KINGS_HALL)
+        if not res:
+            break
+
